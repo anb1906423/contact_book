f7304e82712f362c633192e64777308101b97f7b
unknown <anb1906423@student.ctu.edu.vn>
2022-03-02 22:30:22 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
 
-app.use(express.json());
+const setupContactRoutes = require('./app/routes/contact.routes')
+const { BadRequestError, errorHandler } = require('./app/errors')
+const app = express()
 
-app.use(express.urlencoded({ extended: true }));
+app.ust(cors())
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    // res.json({ message: "Welcome to contact book application." });
-    res.send('Welcome to homepage!');
-});
+    res.json({ message: "Welcome to contact book application." })
+    res.send('Welcome to homepage!')
+})
 
-module.exports = app;
+setupContactRoutes(app)
+
+app.use((req, res, next)) => {
+    next(new BadRequestError(404, 'Resource not found'))
+}
+
+app,use((err, req, res, next)) => {
+    errorHandler.handleError(error, res)
+}
+
+module.exports = app
